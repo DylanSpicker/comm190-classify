dd007d1489e21bf3a3eaf81a1154fdc5adea18e4
Dylan Spicker <dylan.spicker@gmail.com>
2017-07-24 15:48:58 -0400

    Initial commit

---
+++ welcome.py
@@ -0,0 +1,41 @@
+import os
+from flask import Flask, jsonify, request
+from watson_developer_cloud import NaturalLanguageClassifierV1
+
+natural_language_classifier = NaturalLanguageClassifierV1(
+  username='3a7e240e-b28e-46b0-aa6c-6a59310ea203',
+  password='EG7xOCiiL8uq')
+
+classifier_id = '359f41x201-nlc-198247'
+
+app = Flask(__name__)
+
+# Define the Homepage Route
+# This will simply return the static HTML file
+#   which contains the front-end application
+@app.route('/')
+def Welcome():
+    # When the user visits http://hostname.mybluemix.net/ 
+    #   return the static file kept at: /static/index.html
+    return app.send_static_file('index.html')
+
+# Define the Analyze Route
+# This will take the text submitted by the users
+#   pass it along to the Watson NLC analysis SDK and 
+#   return the results of the call
+@app.route('/analyze', methods=['POST', 'GET'])
+def AnalyzeRequest():
+    # Get the Request Data
+    comment_text = request.form['text']
+
+    # Make a classification request
+    # Simply call the "classes" method on the "natural_language_classifier" object created at the outset
+    classes = natural_language_classifier.classify(classifier_id, comment_text)
+
+    # Return the classes; jsonify simply re-encodes the data in a means
+    #   that is easy to parse using the JavaScript file
+    return jsonify(classes)
+
+port = os.getenv('PORT', '5000')
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=int(port), debug=True)
